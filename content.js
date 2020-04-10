58312fb9a3aa4df61b23fb43589671188812dc36
Kellis <34947108+Gumbraise@users.noreply.github.com>
2020-04-10 14:03:40 +0200

    modify

--- content.js
+++ content.js
@@ -1,5 +1,23 @@
-var data = document.getElementsByTagName('html')[0]; 
+var images = document.getElementsByTagName('img'); 
 
-var data_new = data.innerHTML.replace(/Create/gi, 'lorem ipsum'); 
+/*var requestJS = new XMLHttpRequest();
+var requestURL = 'emojis.json';
+requestJS.open('GET', requestURL);
+requestJS.responseType = 'json';
+requestJS.send();
 
-data.innerHTML = data_new;
+requestJS.onload = function() {
+    var count = 0;
+    for (var key in requestJS.response) {
+        count++;
+        data.innerHTML = requestJS.response['😓']
+
+    }
+    
+}
+*/
+
+for (var i = 0, l = images.length; i < l; i++) {
+    var data_new = images[i].src.replace(/https:\/\/fonts.gstatic.com\/s\/e\/notoemoji\/latest\/1f497\/emoji.svg/gi, 'heyy')
+    images[i].src = 'hey'
+}
